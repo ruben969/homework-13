2a7ade61fad0fd305ba6541856b42441a33fbf4c
Ruben Arzoyan <ruben.arzoyan@gmail.com>
2018-10-15 22:03:24 +0400

    create Task, new Task(shablon :)) )

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Task from './Components/Task';
+import List from './Components/List';
+import NewTask from './Components/new-task'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div >
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <Task className="App"/>
+         
         </header>
       </div>
     );
