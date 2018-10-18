a5a2752116d323440bdae56cff1fd130ecc15e8a
Ruben Arzoyan <ruben.arzoyan@gmail.com>
2018-10-18 15:27:46 +0400

    add-edit function,

--- src/App.js
+++ src/App.js
@@ -3,16 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 import Task from './Components/Task';
 import List from './Components/List';
-import NewTask from './Components/new-task'
+import NewTask from './Components/new-task';
 
 class App extends Component {
   render() {
     return (
-      <div >
-        <header className="App-header">
-          <Task className="App"/>
-         
-        </header>
+      <div className='app-todo' >
+          <Task className='App'/>
+          <List/>
+        
       </div>
     );
   }
