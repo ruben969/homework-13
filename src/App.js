61cf9968824e7171f42a724e000194b1f873f53f
Ruben Arzoyan <ruben.arzoyan@gmail.com>
2018-10-19 22:33:12 +0400

    finli commit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,42 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Task from './Components/Task';
-import List from './Components/List';
 import NewTask from './Components/new-task';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.titleName = this.titleName.bind(this);
+    this.titleCreate = this.titleCreate.bind(this);
+
+    this.state = {
+      title: '',
+      arrTitle: [],
+    };
+
+  }
+
+  titleName(event) {
+    this.setState({ title: event.target.value });
+  }
+
+  titleCreate() {
+    this.state.arrTitle.push({
+      listTitle: this.state.title,
+    });
+
+    this.setState({title: ``})
+  }
+
   render() {
+    const data = this.state.arrTitle;
     return (
       <div className='app-todo' >
-          <Task className='App'/>
-          <List/>
-        
+        <Task className='App' list={this.state} titleName={this.titleName} titleCreate={this.titleCreate}/>
+        <NewTask data={data} />
       </div>
     );
   }
 }
-
 export default App;
